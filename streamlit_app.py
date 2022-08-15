9e0a9e4917b65d3d52c0e857fd1b5db6ebd8083f
Alejandro Maximiliano Balderas Tapia <111298297+abalderast@users.noreply.github.com>
2022-08-15 10:51:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healty Dinner")
+streamlit.Header("Breakfast Menu")
+streamlit.Text("Omega 3 & Blueberry Oatmeal")
+streamlit.Text("Kale")
+streamlit.Text("Hard Boiled")
