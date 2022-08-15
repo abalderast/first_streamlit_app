2a3d8e4f10818c8f2cd35c6d79032cbd472102bd
Alejandro Maximiliano Balderas Tapia <111298297+abalderast@users.noreply.github.com>
2022-08-15 10:56:05 -0500

    Update streamlit_app.py
    Correct 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("My Parents New Healty Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale")
-streamlit.text("Hard Boiled")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
