d3da1aeded644fbd723c94c36663edd03aec5225
Alejandro Maximiliano Balderas Tapia <111298297+abalderast@users.noreply.github.com>
2022-08-16 15:52:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? ? creates the table on the page
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 #import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
